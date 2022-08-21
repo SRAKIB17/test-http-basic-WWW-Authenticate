f681d66673a1b05d9531a29c9ad002bb75470017
SRAKIB17 <rakibulssc5@gmail.com>
2022-08-21 21:09:07 +0600

    'test'

--- index.js
+++ index.js
@@ -8,20 +8,19 @@ app.use(express.json())
 
 app.get('/', (req, res) => {
 
-    // const userpass = Buffer.from(
-    //     (req.headers.authorization || '').split(' ')[1] || '',
-    //     'base64'
-    // ).toString();
+    const userpass = Buffer.from(
+        (req.headers.authorization || '').split(' ')[1] || '',
+        'base64'
+    ).toString();
 
 
-    // if (userpass !== 'rakib:123456') {
-    //     res.writeHead(401, { 'WWW-Authenticate': 'Basic' });
-    //     return res.end('HTTP Error 401 Unauthorized: Access is denied');
-    // }
-    // else {
-    //     return res.end('You are in! Yay!!');
-    // }
-    res.end('You are in! Yay!!');
+    if (userpass !== 'rakib:123456') {
+        res.writeHead(401, { 'WWW-Authenticate': 'Basic' });
+        return res.end('HTTP Error 401 Unauthorized: Access is denied');
+    }
+    else {
+        return res.end('You are in! Yay!!');
+    }
 })
 
 app.listen(port, () => {
