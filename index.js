3104e9048ba9f0c713313ff79e6d57ab2fb45571
SRAKIB17 <rakibulssc5@gmail.com>
2022-08-22 04:29:31 +0600

    'done'

--- index.js
+++ index.js
@@ -7,21 +7,62 @@ var fs = require('fs');
 const app = express()
 app.use(express.json())
 
-app.get('/', (req, res) => {
+app.post('/', (req, res) => {
 
     const userpass = Buffer.from(
         (req.headers.authorization || '').split(' ')[1] || '',
         'base64'
     ).toString();
 
+    if (userpass) {
+        const body = req.body || ''
 
-    if (userpass !== 'rakib:123456') {
-        res.writeHead(401, { 'WWW-Authenticate': 'Basic' });
-        return res.end('HTTP Error 401 Unauthorized: Access is denied');
+        const data = JSON.stringify(body)
+
+        fs.writeFile('db.json', data, function (err) {
+
+            if (err) {
+                return console.error(err);
+            }
+
+            fs.readFile('db.json', function (err, data) {
+                if (err) {
+                    return console.error(err);
+                }
+                return res.end(data?.toString());
+            });
+        });
+        console.log(userpass)
+        return res.send('You are in! Yay!!');
     }
-    else {
-        return res.end('You are in! Yay!!');
+
+
+    res.writeHead(401, { 'WWW-Authenticate': 'Basic' });
+    res.end('HTTP Error 401 Unauthorized: Access is denied');
+})
+
+
+app.get('/', async (req, res) => {
+
+    const userpass = Buffer.from(
+        (req.headers.authorization || '').split(' ')[1] || '',
+        'base64'
+    ).toString();
+
+    if (userpass) {
+        console.log(userpass)
+        await fs.readFile('db.json', await function (err, data) {
+            if (err) {
+                return console.error(err);
+            }
+            return res.end(data?.toString());
+        });
+        return res.end('sdflsdfjl');
     }
+
+
+    res.writeHead(401, { 'WWW-Authenticate': 'Basic' });
+    res.end('HTTP Error 401 Unauthorized: Access is denied');
 })
 
 app.get('/fs', (req, res) => {
