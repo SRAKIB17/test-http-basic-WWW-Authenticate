12b9fc64469f877ffb5b3f677124b2fb3a951164
SRAKIB17 <rakibulssc5@gmail.com>
2022-08-21 21:07:49 +0600

    'test'

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
-const port = 5000 || process.env.PORT;
+const port = process.env.PORT || 5000;
 
 
 const app = express()
-
+app.use(express.json())
 
 app.get('/', (req, res) => {
 
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
     // else {
     //     return res.end('You are in! Yay!!');
     // }
-    return res.end('You are in! Yay!!');
+    res.end('You are in! Yay!!');
 })
 
 app.listen(port, () => {
